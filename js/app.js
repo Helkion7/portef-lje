1f0fad14e4127dfa6ee986780460b0d286538fa1
Lisan-al-gaib <helkion@proton.me>
2024-08-30 13:44:59 +0200

    added main piece

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// Show the button when scrolling down 20px from the top of the document
 window.onscroll = function() {
     const scrollTopBtn = document.getElementById("scrollTopBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -8,7 +7,6 @@ window.onscroll = function() {
     }
 };
 
-// Scroll to the top of the page when the button is clicked
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
