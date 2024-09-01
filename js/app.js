4bc1e58ad7f8a29ce3e8de6051d778d997fc20a5
Brynjar berg Ødegaard <helkion@proton.me>
2024-09-01 20:41:15 +0200

    begun upon the project cards which will expand to full screen and project the project

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
-window.onscroll = function() {
-    const scrollTopBtn = document.getElementById("scrollTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollTopBtn.style.display = "flex";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
-};
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+function displayProject(event) {
+  // Add the 'projectCardExpanded' class to the clicked element
+  event.target.classList.add("projectCardExpanded");
 }
